590a1f6c5be641aded68fd23427c1a9e3613cfd8
Vlah <brennan.vlahcevic@armssoftware.com>
2022-06-23 00:12:42 -0400

    finished high score logging & high scores page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,170 +1,74 @@
-// Timer Stuff
-    // 75 seconds to start
-    // When incorrect subtract 15 seconds
-    // Final Score is time remaining
+// Questions
+var questions = [
+    {
+        question: "What pirate is most likely to use 3rd party APIs?",
+        answers: [
+            "1. Jack Sparrow",
+            "2. Bootstrap Bill Turner",
+            "3. Blackbeard",
+            "4. Long John Silver"
+        ],
+        correctAnswer: "1"
+    },
+    {
+        question: "Jesus Christ it's ______ Bourne.",
+        answers: [
+            "1. James",
+            "2. Jackson",
+            "3. JSON",
+            "4. Jason"
+        ],
+        correctAnswer: "3"
+    },
+    {
+        question: "What is the second film in a series of movies called?",
+        answers: [
+            "1. Sequel",
+            "2. Part 2: Electic Boogaloo",
+            "3. A Box Office Bomb",
+            "4. SQL"
+        ],
+        correctAnswer: "0"
+    },
+    {
+        question: "Question",
+        answers: [
+            "1. 2",
+            "2. 3",
+            "3. 4",
+            "4. 1"
+        ],
+        correctAnswer: "1"
+    },
+    {
+        question: "What grade am I likely to get on this challenge?",
+        answers: [
+            "1. A+",
+            "2. B",
+            "3. C++",
+            "4. F-"
+        ],
+        correctAnswer: "2"
+    }
+];
 
+// Global Variables
 var timerEl = document.getElementById('timer');
 var questionsEl = document.getElementById('questions');
 var headerEl = document.getElementById('question');
 var instructionsEl = document.getElementById('instructions');
 var correctEl = document.getElementById('correct');
+var resultBoxEl = document.getElementById('result');
 var formEl = document.getElementById('score-form');
+var highScoresEl = document.getElementById('high-scores');
 
 var questionIdCounter = 0;
+var playerScore = "";
 var timeLeft = 75;
-
-var questions = [
-    {
-        question: "Commonly used data types do NOT include:",
-        answers: [
-            "1. strings",
-            "2. booleans",
-            "3. alerts",
-            "4. integers"
-        ],
-        correctAnswer: "2"
-    },
-    {
-        question: "The condition in an if/else statement is enclosed with ______.",
-        answers: [
-            "1. quotes",
-            "2. curly brackets",
-            "3. parenthesis",
-            "4. square brackets"
-        ],
-        correctAnswer: "2"
-    },
-    {
-        question: "Arrays in JavaScript can be used to store ______.",
-        answers: [
-            "1. numbers and strings",
-            "2. other arrays",
-            "3. booleans",
-            "4. all of the above"
-        ],
-        correctAnswer: "3"
-    },
-    {
-        question: "String values must be enclosed within ______ when being assigned to variables.",
-        answers: [
-            "1. commas",
-            "2. curly brackets",
-            "3. quotes",
-            "4. parenthesis"
-        ],
-        correctAnswer: "2"
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: [
-            "1. JavaScript",
-            "2. terminal/bash",
-            "3. for loops",
-            "4. console.log"
-        ],
-        correctAnswer: "3"
-    }
-];
-
-var submitScore = function () {
-
-    // Stop Timer and Set Score
-    var score = timeLeft;
-    timerEl.textContent = score;
-    clearInterval(timeInterval);
-
-    // Remove Last Buttons and Correct/Incorrect Text
-    for (var i=0; i < questions[questions.length-1].answers.length; i++) {
-        var removeButtonEl = document.querySelector(".answer-choice[data-id='" + i + "']");
-        removeButtonEl.innerHTML = "";
-    };
-
-    // Update Page Text
-    headerEl.innerHTML = "All Done"
-    instructionsEl.className = "instructions";
-    instructionsEl.innerHTML = "Your final score is " + score;
-
-    // Create Form
-    formEl.innerHTML ="<p class='instructions'>Enter Initials: </p><input type='text' name='player-initials' />"
-    console.log(formEl);
-};
+var savedScores = [];
 
 
-// Timer
-function timer() {    
-    timeInterval = setInterval(function (){
-        if (timeLeft >0) {
-            timerEl.textContent = timeLeft
-            timeLeft--;
-        }
-        else {
-            timerEl.textContent = 0;
-            clearInterval(timeInterval);
-
-            submitScore();
-        };
-    }, 1000);
-};
-
-// Question Control
-var addQuestion = function() {
-    if (questionIdCounter < questions.length) {
-        // Update Page Text
-        headerEl.className = "question-heading";
-        headerEl.innerHTML = questions[questionIdCounter].question;
-        // questionsEl.appendChild(headerEl);
-
-        instructionsEl.innerHTML = "";
-        
-        // Create Answer Choices
-        for (var i=0; i < questions[questionIdCounter].answers.length; i++) {
-            var answersEl = document.querySelector(".answer-choice[data-id='" + i + "']");
-            answersEl.innerHTML = "<button class='btn answer-button' data-task-id=" + i +">" + questions[questionIdCounter].answers[i] + "</button>";
-        };
-    }
-    else {
-        submitScore();
-    }
-};
-
-var quizButtonHandler = function(event) {
-    if (event.target.matches(".start-btn")) {
-        // remove start button
-        questionsEl.removeChild(questionsEl.lastChild);
-        
-        // Start Quiz
-        timer();
-        addQuestion();
-    }
-    else if (event.target.matches(".answer-button")) {
-
-        var answerId = event.target.getAttribute("data-task-id");
-        
-        // Determine if Correct or Incorrect answer choice
-        if (answerId === questions[questionIdCounter].correctAnswer) {
-            //
-            correctEl.innerHTML = "Correct!"
-
-            var myTimer = setInterval(function (){
-                correctEl.innerHTML = ""
-            }, 2000);
-        }
-        else {
-            correctEl.innerHTML = "Wrong!"
-
-            var myTimer = setInterval(function (){
-                correctEl.innerHTML = ""
-            }, 2000);
-
-            timeLeft -= 10;
-        };
-
-        // Move to Next Question
-        questionIdCounter++;
-        addQuestion();
-    };
-};
-
+// Load Starting Screen
 var startScreenEl = function() {
     // Update header
     headerEl.className = "center question-heading";
@@ -183,6 +87,161 @@ var startScreenEl = function() {
     questionsEl.appendChild(startButtonEl);
 };
 
-questionsEl.addEventListener("click", quizButtonHandler);
-// formEl.addEventListener("submit", recordHighScore);
+// Start Timer
+function timer() {    
+    timeInterval = setInterval(function (){
+        if (timeLeft >0) {
+            timerEl.textContent = timeLeft
+            timeLeft--;
+        }
+        else {
+            timerEl.textContent = 0;
+            clearInterval(timeInterval);
+
+            submitScore();
+        };
+    }, 1000);
+};
+
+// Button Handler
+var buttonHandler = function(event) {
+    event.preventDefault();
+    // Start Button
+    if (event.target.matches(".start-btn")) {
+
+        // clean page
+        questionsEl.removeChild(questionsEl.lastChild);
+        instructionsEl.innerHTML = "";
+        
+        // Start Quiz
+        timer();
+        addQuestion();
+    }
+
+    // Answer Choices
+    else if (event.target.matches(".answer-button")) {
+        var answerId = event.target.getAttribute("data-task-id");
+        
+        // Determine if Correct or Incorrect answer choice
+        if (answerId === questions[questionIdCounter].correctAnswer) {
+            correctEl.innerHTML = "Correct!"
+        }
+        else {
+            correctEl.innerHTML = "Wrong!"
+
+            timeLeft -= 10;
+        };
+
+        // Move to Next Question
+        questionIdCounter++;
+        addQuestion();
+    }
+
+    // Score Submission Button
+    else if (event.target.matches(".score-btn")) {
+        saveScore();
+    }
+};
+
+// Generate Questions
+var addQuestion = function() {
+    if (questionIdCounter < questions.length) {
+        // Update Page Text
+        headerEl.className = "question-heading";
+        headerEl.innerHTML = questions[questionIdCounter].question;
+        
+        // Create Answer Choices
+        for (var i=0; i < questions[questionIdCounter].answers.length; i++) {
+            var answersEl = document.querySelector(".answer-choice[data-id='" + i + "']");
+            answersEl.innerHTML = "<button class='btn answer-button' data-task-id=" + i +">" + questions[questionIdCounter].answers[i] + "</button>";
+        };
+    }
+    else {
+        submitScore();
+    }
+};
+
+// Submit Score Page Setup
+var submitScore = function () {
+
+    // Stop Timer and Set Score
+    playerScore = timeLeft;
+    timerEl.textContent = playerScore;
+    clearInterval(timeInterval);
+
+    // Remove Last Buttons and Correct/Incorrect Text
+    for (var i=0; i < questions[questions.length-1].answers.length; i++) {
+        var removeButtonEl = document.querySelector(".answer-choice[data-id='" + i + "']");
+        removeButtonEl.innerHTML = "";
+    };
+
+    // Update Page Text
+    headerEl.innerHTML = "All Done"
+    instructionsEl.className = "instructions";
+    instructionsEl.innerHTML = "Your final score is " + playerScore + "!";
+    correctEl.innerHTML = ""
+
+    // Create Form
+    formTextEl = document.getElementById('form-text');
+    formInitialsEl = document.getElementById('form-initials');
+    formSubmitEl = document.getElementById('form-submit');
+
+    formTextEl.innerHTML = "<p class='form-text'>Type Initials: </p>"
+    formInitialsEl.innerHTML = "<input type='text' name='initials' placeholder='Enter Initials' />"
+    formSubmitEl.innerHTML = "<button class='btn score-btn' id='submit-score' type='submit'>Submit</button>"
+};
+
+// Save Scores to Local Storage
+var saveScore = function() {
+    var initialsInput = document.querySelector("input[name='initials']").value;
+    savedScore = {Initials: initialsInput, Score: playerScore};
+    savedScores.push(savedScore);
+    localStorage.setItem("savedScores", JSON.stringify(savedScores));
+
+    highScores();
+};
+
+// Prevent default browser action on High Scores Button click
+var highScoreButton = function(event) {
+    event.preventDefault();
+    highScores();
+}
+// Pull scores into and create High Scores table
+var highScores = function() {
+    //Clean up Page
+    formEl.innerHTML="";
+    headerEl.className = "center question-heading";
+    headerEl.innerHTML = "High Scores";
+    instructionsEl.innerHTML = "";
+    timeLeft = "";
+
+    var scoreListEl = document.createElement("ol");
+    questionsEl.appendChild(scoreListEl);
+
+    // Pull Scores from Local Server
+    var pulledScores = localStorage.getItem("savedScores");
+    if (!pulledScores) {
+        return false;
+    }
+    pulledScores = JSON.parse(pulledScores);
+
+    // sort scores descending
+    pulledScores.sort(function(a,b) {
+        return b.Score - a.Score
+    });
+
+
+    //Parse Scores
+    for (var i=0; i < pulledScores.length; i++) {
+        var scoresEl = document.createElement("li");
+        scoresEl.setAttribute("class","high-scores");
+        scoresEl.innerHTML = pulledScores[i].Initials + " - " + pulledScores[i].Score;
+        scoreListEl.appendChild(scoresEl);
+    }
+
+}
+
+// Default Function Calls and Event Listeners
+questionsEl.addEventListener("click", buttonHandler);
+highScoresEl.addEventListener("click", highScoreButton);
 startScreenEl();
